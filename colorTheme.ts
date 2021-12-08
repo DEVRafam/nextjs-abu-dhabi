0335edcfea404fa51c92f587659c483be175ec22
DEVRafam <jodax95@gmail.com>
2021-12-08 17:15:28 +0100

    🐛 FIX: Register- new packages added

--- colorTheme.ts
+++ colorTheme.ts
@@ -1,11 +1,29 @@
 import { createTheme, responsiveFontSizes } from "@mui/material/styles";
 
+declare module "@mui/material/styles" {
+    interface Palette {
+        neutral: Palette["primary"];
+    }
+    interface PaletteOptions {
+        neutral: PaletteOptions["primary"];
+    }
+}
+declare module "@mui/material/Button" {
+    interface ButtonPropsColorOverrides {
+        neutral: true;
+    }
+}
+
 export default responsiveFontSizes(
     createTheme({
         palette: {
             mode: "dark",
             primary: {
                 main: "#FDA7DF",
+            },
+            neutral: {
+                main: "#222f3e",
+                contrastText: "#fff",
             },
         },
     })
