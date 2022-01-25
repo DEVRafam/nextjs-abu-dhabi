7b2814dd1619e56807b05a6c68b36b979440627e
DEVRafam <jodax95@gmail.com>
2022-01-25 08:50:31 +0100

    👌 IMPROVE: Cypress new global functions

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -12,6 +12,12 @@ declare global {
              * @example cy.dataCy('greeting')
              */
             getByCyTag(value: string): Chainable<JQuery<HTMLElement>>;
+            /**
+             * Custom command to ensure that user can use right side button to toggle the visibility of entering password.
+             * @param {string} **cyTag** -  `data-cy` attribute of password element to test
+             * @example cy.checkPasswordElementVisibility('greeting')
+             */
+            checkPasswordElementVisibility(cyTag: string): void;
         }
     }
 }
