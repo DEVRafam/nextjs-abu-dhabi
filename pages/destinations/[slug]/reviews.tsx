eb2be13390947bfb4b80fcabccca9632273ae96d
DEVRafam <jodax95@gmail.com>
2022-03-08 02:19:06 +0100

    👌 IMPROVE: Destinations' routes

---
+++ pages/destinations/[slug]/reviews.tsx
@@ -0,0 +1,17 @@
+// Tools
+// Types
+import type { FunctionComponent } from "react";
+
+interface CertinDestinationReviewsProps {
+    //
+}
+
+const CertinDestinationReviews: FunctionComponent<CertinDestinationReviewsProps> = (props) => {
+    return (
+        <>
+            <span>REVIEWS</span>
+        </>
+    );
+};
+
+export default CertinDestinationReviews;
