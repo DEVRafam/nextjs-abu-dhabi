e7e0bb3458289fd04625fb117ce3d8b68717a7bb
DEVRafam <jodax95@gmail.com>
2022-05-26 23:26:20 +0200

    👌 IMPROVE: Test utils/ testPagination - additional tests possibility

--- tests/API/helpers/landmarks/bulk/makeRequest.ts
+++ tests/API/helpers/landmarks/bulk/makeRequest.ts
@@ -3,19 +3,25 @@ import createRequestWithURLQueries from "../../createRequestWithURLQueries";
 // Types
 import type { LandmarkType, Continent } from "@prisma/client";
 import type { Landmark } from "@/@types/pages/landmarks/ManyLandmarks";
+import type { PaginationProperties } from "@/@types/pages/api/Pagination";
 
 export interface MakeRequestParams {
     certainLandmarkType?: LandmarkType;
     searchingPhrase?: string;
     continent?: Continent;
-    order?: "asc" | "desc";
+    sort?: "asc" | "desc";
     orderBy?: "createdAt";
     page?: number;
     perPage?: number;
 }
 
+export interface Response {
+    data: Landmark[];
+    pagination: PaginationProperties;
+}
+
 // eslint-disable-next-line import/no-anonymous-default-export
-export default createRequestWithURLQueries<MakeRequestParams, Landmark>({
+export default createRequestWithURLQueries<MakeRequestParams, Response>({
     url: "/api/landmark/bulk",
-    possibleURLQueries: ["certainLandmarkType", "continent", "order", "searchingPhrase", "orderBy", "page", "perPage"],
+    possibleURLQueries: ["certainLandmarkType", "continent", "sort", "searchingPhrase", "orderBy", "page", "perPage"],
 });
--- tests/API/helpers/testPagination.ts
+++ tests/API/helpers/testPagination.ts
@@ -1,14 +1,9 @@
 /* eslint-disable import/no-anonymous-default-export */
 import type { PaginationProperties } from "@/@types/pages/api/Pagination";
 
-type PieceOfData = {
-    slug: string;
-};
-
 interface TestPaginationParams {
     /**
-     * **ASYNC** callback returning a dataset in which each record has a `SLUG` property
-     * ---
+     * Async callback returning all posible data of this collection
      * ```ts
      *  async () =>
      *      await prisma.landmark.findMany({
@@ -21,31 +16,46 @@ interface TestPaginationParams {
      *      }),
      * ```
      */
-    getAllAvailableData: () => Promise<(Record<any, any> & PieceOfData)[]>;
+    getAllAvailableData: () => Promise<Record<any, any>[]>;
     /**
      * **ASYNC** callback returning data on particular page. This function accepts one parameter-
      * the number of page to load data from.
-     * ---
      * ```ts
-     * async (page: number) =>
+     * async (page: number, perPage:number) =>
      *      await makeRequest({
      *          certainLandmarkType: "BUILDING",
      *          page,
-     *          perPage: 2,
+     *          perPage,
      *      }),
      * ```
      */
-    loadPage: (page: number) => Promise<{
-        data: { slug: string }[];
+    loadPage: (
+        page: number,
+        perPage: number
+    ) => Promise<{
+        data: any[];
         pagination: PaginationProperties;
     }>;
+    /** Something unique differentiating each individual record */
+    uniquePropertyName?: string;
+    /** Array containging numbers of records per page to test */
+    recordsPerPage: number[];
+    /**
+     * Array of additional tests which will run besides two basic pagination test
+     */
+    additionalTests?: {
+        /** Name of the test- this will be displayed in the terminal */
+        name: string;
+        /** Callback accepting one parameter- data comming from the API request */
+        cb: (data: any[]) => Promise<void>;
+    }[];
 }
 /**
  * **Pagination testing function**
  * The purpose of this function is to test all pages and ensure,
  * that **every** available record is displayed and no records are repeating
  * throughout different pages
- * ---
+ *
  * ```ts
  *  testPaginations({
  *      getAllAvailableData: async () =>
@@ -57,58 +67,85 @@ interface TestPaginationParams {
  *                  slug: true,
  *              },
  *          }),
- *      loadPage: async (page: number) =>
+ *      loadPage: async (page: number, perPage:number) =>
  *          await makeRequest({
  *              certainLandmarkType: "BUILDING",
  *              page,
- *              perPage: 2,
+ *              perPage,
  *          }),
  *  });
  *
  * ```
  */
 export default (params: TestPaginationParams) => {
-    const { getAllAvailableData, loadPage } = params;
+    const { getAllAvailableData, loadPage: _loadPage, uniquePropertyName: _uniquePropertyName, recordsPerPage, additionalTests } = params;
+    const uniquePropertyName = _uniquePropertyName ?? "slug";
+    /**
+     * Just a shourtcut for `(el as any)[uniqueProperty]`
+     */
+    const getUniquePropery = (el: any) => el[uniquePropertyName];
 
     describe("Data can be paginated", () => {
-        test("All possible data should be eventually displayed", async () => {
-            const uniqueSlugs: Set<string> = new Set();
-            const { data, pagination } = await loadPage(1);
-            const allAvailableData = await getAllAvailableData();
+        for (const perPage of recordsPerPage) {
+            const loadPage = async (page: number) => await _loadPage(page, perPage);
 
-            const storeAllIDs = (data: PieceOfData[]) => data.forEach((el: PieceOfData) => uniqueSlugs.add(el.slug));
-            //
-            storeAllIDs(data);
-            // Load all remaining pages
-            for (let page = 2; page <= pagination.pagesInTotal; page++) {
-                const { data } = await loadPage(page);
-                storeAllIDs(data);
-            }
+            describe(`${perPage} records per page`, () => {
+                test("All possible data should be eventually displayed", async () => {
+                    const uniquePropertyCollection: Set<string> = new Set();
+                    const { data, pagination } = await loadPage(1);
+                    const allAvailableData = await getAllAvailableData();
 
-            expect(allAvailableData).toHaveLength(uniqueSlugs.size);
-            allAvailableData.forEach((el) => {
-                expect(uniqueSlugs).toContain(el.slug);
-            });
-        });
-        test("Records do not repeat throughout diffrent pages", async () => {
-            const uniqueSlugs: Set<string> = new Set();
-            const { data, pagination } = await loadPage(1);
-            const allAvailableData = await getAllAvailableData();
+                    const storeAllIDs = (data: any[]) => {
+                        for (const el of data) uniquePropertyCollection.add(getUniquePropery(el));
+                    };
+                    storeAllIDs(data);
 
-            data.forEach((el: PieceOfData) => uniqueSlugs.add(el.slug));
-            // Load all remaining pages
-            for (let page = 2; page <= pagination.pagesInTotal; page++) {
-                const { data } = await loadPage(page);
-                data.forEach((el: PieceOfData) => {
-                    expect(uniqueSlugs).not.toContain(el.slug);
-                    uniqueSlugs.add(el.slug);
+                    for (let page = 2; page <= pagination.pagesInTotal; page++) {
+                        const { data } = await loadPage(page);
+                        storeAllIDs(data);
+                    }
+
+                    expect(allAvailableData).toHaveLength(uniquePropertyCollection.size);
+                    allAvailableData.forEach((el) => {
+                        expect(uniquePropertyCollection).toContain(el[uniquePropertyName]);
+                    });
                 });
-            }
+                test("Records do not repeat throughout diffrent pages", async () => {
+                    const uniquePropertyCollection: Set<string> = new Set();
+                    const allAvailableData = await getAllAvailableData();
+                    const { data, pagination } = await loadPage(1);
 
-            expect(allAvailableData).toHaveLength(uniqueSlugs.size);
-            allAvailableData.forEach((el) => {
-                expect(uniqueSlugs).toContain(el.slug);
+                    data.forEach((el) => {
+                        uniquePropertyCollection.add(getUniquePropery(el));
+                    });
+
+                    for (let page = 2; page <= pagination.pagesInTotal; page++) {
+                        const { data } = await loadPage(page);
+                        data.forEach((el) => {
+                            expect(uniquePropertyCollection).not.toContain(getUniquePropery(el));
+                            uniquePropertyCollection.add(getUniquePropery(el));
+                        });
+                    }
+
+                    expect(allAvailableData).toHaveLength(uniquePropertyCollection.size);
+                    allAvailableData.forEach((el) => {
+                        expect(uniquePropertyCollection).toContain(getUniquePropery(el));
+                    });
+                });
+
+                if (additionalTests) {
+                    for (const additionalTest of additionalTests) {
+                        test(additionalTest.name, async () => {
+                            const { data, pagination } = await loadPage(1);
+                            await additionalTest.cb(data);
+                            for (let page = 2; page <= pagination.pagesInTotal; page++) {
+                                const { data } = await loadPage(page);
+                                await additionalTest.cb(data);
+                            }
+                        });
+                    }
+                }
             });
-        });
+        }
     });
 };
