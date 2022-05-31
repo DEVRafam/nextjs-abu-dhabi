9ab79f818934d7ee5a3c8162396cd0db2186e15c
DEVRafam <jodax95@gmail.com>
2022-05-31 10:56:01 +0200

    👌 IMPROVE: Prisma / destination seeder

--- prisma/data/destinations.ts
+++ prisma/data/destinations.ts
@@ -1,8 +1,19 @@
 // Tools
 import { generateDescriptionHeader, generateShortDescription, generateDescriptionParagraph, generateDescriptionSplittedParagraph } from "./_prisma_seeders_utils";
 // Types
-import type { SeederDataList, Destination } from "./@types";
+import type { SeederDataList, Destination, SeederDataItem } from "./@types";
 import type { DescriptionContentField, HeaderContentField, ParagraphContentField, ImageContentField, SplittedContentField } from "../../@types/Description";
+import { Continent } from "@prisma/client";
+
+interface ShortenDestination {
+    id: string;
+    city: string;
+    continent: Continent;
+    country: string;
+    countryCode: string;
+    population: number;
+    slug: string;
+}
 
 const createDescription = () => {
     return JSON.parse(
@@ -56,110 +67,83 @@ const createDescription = () => {
     );
 };
 
-export default [
-    {
-        id: "KRAKOW",
-        city: "Kraków",
-        continent: "Europe",
-        country: "Poland",
-        countryCode: "pl",
-        folder: "krakow",
-        city_lowercase: "kraków",
-        country_lowercase: "poland",
-        _imagesDir: "destinations/krakow",
-        population: 700000,
-        shortDescription: generateShortDescription(),
-        slug: "krakow",
-        description: createDescription(),
-    },
-    {
-        id: "WARSZAWA",
-        city: "Warszawa",
-        continent: "Europe",
-        country: "Poland",
-        countryCode: "pl",
-        folder: "warszawa",
-        city_lowercase: "warszawa",
-        country_lowercase: "poland",
-        _imagesDir: "destinations/warszawa",
-        population: 1700000,
-        shortDescription: generateShortDescription(),
-        slug: "wwa",
-        description: createDescription(),
-    },
-    {
-        id: "VANCOUVER",
-        city: "Vancouver",
-        continent: "North_America",
-        country: "Canada",
-        countryCode: "ca",
-        folder: "vancouver",
-        city_lowercase: "vancouver",
-        country_lowercase: "canada",
-        _imagesDir: "destinations/vancouver",
-        population: 675218,
-        shortDescription: generateShortDescription(),
-        slug: "vancouver",
-        description: createDescription(),
-    },
-    {
-        id: "RIO_DE_JANEIRO",
-        city: "Rio de Janeiro",
-        continent: "South_America",
-        country: "Brazil",
-        countryCode: "br",
-        folder: "rio_de_janeiro",
-        city_lowercase: "rio de janeiro",
-        country_lowercase: "brazil",
-        _imagesDir: "destinations/rio_de_janeiro",
-        population: 6752180,
-        shortDescription: generateShortDescription(),
-        slug: "rio_de_janeiro",
-        description: createDescription(),
-    },
-    {
-        id: "CAPE_TOWN",
-        city: "Cape Town",
-        continent: "Africa",
-        country: "South Africa",
-        countryCode: "za",
-        folder: "cape_town",
-        city_lowercase: "cape town",
-        country_lowercase: "south africa",
-        _imagesDir: "destinations/cape_town",
-        population: 4752180,
-        shortDescription: generateShortDescription(),
-        slug: "cape_town",
-        description: createDescription(),
-    },
-    {
-        id: "HAMBURG",
-        city: "Hamburg",
-        continent: "Europe",
-        country: "Germany",
-        countryCode: "de",
-        folder: "hamburg",
-        city_lowercase: "hamburg",
-        country_lowercase: "germany",
-        _imagesDir: "destinations/hamburg",
-        population: 1852180,
-        shortDescription: generateShortDescription(),
-        slug: "hamburg",
-        description: createDescription(),
-    },
-    {
-        id: "TOKYO",
-        city: "Tokyo",
-        continent: "Asia",
-        country: "Japan",
-        countryCode: "jp",
-        folder: "tokyo",
-        city_lowercase: "tokyo",
-        country_lowercase: "japan",
-        _imagesDir: "destinations/tokyo",
-        population: 13752180,
-        shortDescription: generateShortDescription(),
-        slug: "tokyo",
-        description: createDescription(),
-    },
-].reverse() as SeederDataList<Destination>;
+export default (
+    [
+        {
+            id: "KRAKOW",
+            city: "Kraków",
+            continent: "Europe",
+            country: "Poland",
+            countryCode: "pl",
+            population: 700000,
+            slug: "krakow",
+        },
+        {
+            id: "WARSZAWA",
+            city: "Warszawa",
+            continent: "Europe",
+            country: "Poland",
+            countryCode: "pl",
+            population: 1700000,
+            slug: "warszawa",
+        },
+        {
+            id: "VANCOUVER",
+            city: "Vancouver",
+            continent: "North_America",
+            country: "Canada",
+            countryCode: "ca",
+            population: 675218,
+            slug: "vancouver",
+        },
+        {
+            id: "RIO_DE_JANEIRO",
+            city: "Rio de Janeiro",
+            continent: "South_America",
+            country: "Brazil",
+            countryCode: "br",
+            population: 6752180,
+            slug: "rio_de_janeiro",
+        },
+        {
+            id: "CAPE_TOWN",
+            city: "Cape Town",
+            continent: "Africa",
+            country: "South Africa",
+            countryCode: "za",
+            population: 4752180,
+            slug: "cape_town",
+        },
+        {
+            id: "HAMBURG",
+            city: "Hamburg",
+            continent: "Europe",
+            country: "Germany",
+            countryCode: "de",
+            population: 1852180,
+            slug: "hamburg",
+        },
+        {
+            id: "TOKYO",
+            city: "Tokyo",
+            continent: "Asia",
+            country: "Japan",
+            countryCode: "jp",
+            population: 13752180,
+            slug: "tokyo",
+        },
+    ] as ShortenDestination[]
+)
+    .map((_el) => {
+        const el = _el as unknown as SeederDataItem<Destination>;
+
+        el.folder = el.slug;
+        el.city_lowercase = el.city.toLowerCase();
+        el.country_lowercase = el.country.toLowerCase();
+        el._imagesDir = `destinations/${el.slug}`;
+        el.shortDescription = generateShortDescription();
+        el.description = createDescription();
+
+        return el as any;
+    })
+    .reverse() as SeederDataList<Destination>;
